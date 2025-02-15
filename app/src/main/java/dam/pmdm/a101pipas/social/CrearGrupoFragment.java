c76f2df17e196317d6f28773e34ee4a7f9455c5d
Roberto <roberto.ruiz.personal@gmail.com>
2025-02-15 19:18:02 +0100

    Diseños Crear Grupo y Crear Desafios

---
+++ app/src/main/java/dam/pmdm/a101pipas/social/CrearGrupoFragment.java
@@ -0,0 +1,23 @@
+package dam.pmdm.a101pipas.social;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import dam.pmdm.a101pipas.R;
+
+public class CrearGrupoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crear_grupo, container, false);
+        return view;
+    }
+
+}
